9f37d5d4d9b2984ca1f8a390c4fddcec32290150
sayaka-tsugawa-uhuru <sayaka.tsugawa@uhuru.jp>
2020-05-29 16:26:55 +0900

    first commit

---
+++ build/precache-manifest.67724fa0d40cc0e1d28f1ed8affb5d15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd779efcd942f05bc3d90eac5c4493f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "950d9697319a5de0e8e8",
+    "url": "/static/js/2.9e9f95cb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9e9f95cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84e3986a29a0339e6815",
+    "url": "/static/js/main.38179419.chunk.js"
+  },
+  {
+    "revision": "3cb015972f7e045032d6",
+    "url": "/static/js/runtime-main.f1bc8154.js"
+  }
+]);
